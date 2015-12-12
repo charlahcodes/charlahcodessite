ca62cf991535297d07fc02d6c36f7645e055aa7d
Charlie Miller <charlie@charlah.com>
2015-12-11 21:23:00 -0500

    Added landing page

--- app/js/main.js
+++ app/js/main.js
@@ -9,19 +9,33 @@ var config = function config($stateProvider, $urlRouterProvider) {
   // If routes don't match, go back to homepage
   $urlRouterProvider.otherwise('/');
 
-  $stateProvider.state('root', {
-    templateUrl: 'templates/layout.tpl.html'
-  }). // tells ui-router this is an abstract state (no url)
-  state('root.home', {
+  $stateProvider.state('home', {
     url: '/',
+    // controller: 'HomeController',
     templateUrl: 'templates/home.tpl.html'
-  }). // tells ui-router this is an abstract state (no url)
+  }).state('root', {
+    templateUrl: 'templates/layout.tpl.html'
+  })
+  // .state('root.home', {
+  //   url: '/',
+  //   templateUrl: 'templates/home.tpl.html',
+  //   // tells ui-router this is an abstract state (no url)
+  // })
+  . // tells ui-router this is an abstract state (no url)
   state('root.about', {
     url: '/about',
     // controller: 'HomeController',
     templateUrl: 'templates/about.tpl.html'
-  }).state('root.coding', {
-    url: '/coding',
+  }).state('root.skills', {
+    url: '/skills',
+    // controller: 'HomeController',
+    templateUrl: 'templates/coding.tpl.html'
+  }).state('root.work', {
+    url: '/work',
+    // controller: 'HomeController',
+    templateUrl: 'templates/coding.tpl.html'
+  }).state('root.contact', {
+    url: '/contact',
     // controller: 'HomeController',
     templateUrl: 'templates/coding.tpl.html'
   });
