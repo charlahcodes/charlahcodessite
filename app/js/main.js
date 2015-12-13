a7231ad24b9ae8451cbf596949257fa20f8097f7
Charlie Miller <charlie@charlah.com>
2015-12-13 16:46:44 -0500

    Added temporary landing page

--- app/js/main.js
+++ app/js/main.js
@@ -13,34 +13,38 @@ var config = function config($stateProvider, $urlRouterProvider) {
     url: '/',
     // controller: 'HomeController',
     templateUrl: 'templates/home.tpl.html'
-  }).state('root', {
-    templateUrl: 'templates/layout.tpl.html'
-  })
-  // .state('root.home', {
-  //   url: '/',
-  //   templateUrl: 'templates/home.tpl.html',
-  //   // tells ui-router this is an abstract state (no url)
-  // })
-  . // tells ui-router this is an abstract state (no url)
-  state('root.about', {
-    url: '/about',
-    // controller: 'HomeController',
-    templateUrl: 'templates/about.tpl.html'
-  }).state('root.skills', {
-    url: '/skills',
-    // controller: 'HomeController',
-    templateUrl: 'templates/coding.tpl.html'
-  }).state('root.work', {
-    url: '/work',
-    // controller: 'HomeController',
-    templateUrl: 'templates/coding.tpl.html'
-  }).state('root.contact', {
-    url: '/contact',
-    // controller: 'HomeController',
-    templateUrl: 'templates/coding.tpl.html'
   });
 };
 
+// .state('root', {
+//   templateUrl: 'templates/layout.tpl.html',
+//   // tells ui-router this is an abstract state (no url)
+// })
+// // .state('root.home', {
+// //   url: '/',
+// //   templateUrl: 'templates/home.tpl.html',
+// //   // tells ui-router this is an abstract state (no url)
+// // })
+// .state('root.about', {
+//   url: '/about',
+//   // controller: 'HomeController',
+//   templateUrl: 'templates/about.tpl.html'
+// })
+// .state('root.skills', {
+//   url: '/skills',
+//   // controller: 'HomeController',
+//   templateUrl: 'templates/coding.tpl.html'
+// })
+// .state('root.work', {
+//   url: '/work',
+//   // controller: 'HomeController',
+//   templateUrl: 'templates/coding.tpl.html'
+// })
+// .state('root.contact', {
+//   url: '/contact',
+//   // controller: 'HomeController',
+//   templateUrl: 'templates/coding.tpl.html'
+// });
 config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 exports['default'] = config;
